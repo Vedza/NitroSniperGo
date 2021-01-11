ddcbd351df15c826b5d22afaeff3c7317b963275
quentinvezzani <quentin.vezzani@epitech.eu>
2021-01-11 19:23:47 +0100

    Update release script

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf NitroSniperGo_build_*
 mkdir NitroSniperGo_build_win64 NitroSniperGo_build_linux NitroSniperGo_build_mac
 env GOOS=windows GOARCH=amd64 go build && cp settings.json NitroSniperGo.exe NitroSniperGo_build_win64
 env GOOS=linux go build && cp settings.json NitroSniperGo NitroSniperGo_build_linux
