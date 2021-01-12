69dfc0fa611b6c8127c11668a9ad47e6b47a16ec
quentinvezzani <quentin.vezzani@epitech.eu>
2021-01-12 12:41:58 +0100

    Privnote webhook mistype

--- sniper.go
+++ sniper.go
@@ -383,7 +383,7 @@ func webhookPrivnote(content string, user *discordgo.User, guild string, channel
         	}
 		  ],
 		  "author": {
-			"name": "Giveaway Won !"
+			"name": "Privnote Sniped !"
 		  },
 		  "footer": {
 			"text": "NitroSniperGo made by Vedza"
