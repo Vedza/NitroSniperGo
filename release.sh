89737c6a211ba47b75dcf788893b73db0e9cbc66
quentinvezzani <quentin.vezzani@epitech.eu>
2021-01-26 06:40:00 +0100

    Play sound when Nitro sniped

--- release.sh
+++ release.sh
@@ -5,9 +5,9 @@ if ! .git/hooks/pre-commit; then
 fi
 rm -rf NitroSniperGo_build_*
 mkdir NitroSniperGo_build_win64 NitroSniperGo_build_linux NitroSniperGo_build_mac
-env GOOS=windows GOARCH=amd64 go build && cp settings.json NitroSniperGo.exe NitroSniperGo_build_win64
-env GOOS=linux go build && cp settings.json NitroSniperGo NitroSniperGo_build_linux
-go build && cp NitroSniperGo settings.json NitroSniperGo_build_mac
+env GOOS=windows GOARCH=amd64 go build && cp settings.json settings.json NitroSniperGo.exe NitroSniperGo_build_win64
+env GOOS=linux go build && cp settings.json settings.json NitroSniperGo NitroSniperGo_build_linux
+go build && cp NitroSniperGo settings.json sound.mp3 NitroSniperGo_build_mac
 zip -r NitroSniperGo_build_linux NitroSniperGo_build_linux
 zip -r NitroSniperGo_build_win64 NitroSniperGo_build_win64
 zip -r NitroSniperGo_build_mac NitroSniperGo_build_mac
