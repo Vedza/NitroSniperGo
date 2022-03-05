5fb5e27f504edf4164c508c34b63364df5a1107f
VIRAL69 <100088723+VIRAL69@users.noreply.github.com>
2022-03-05 20:35:08 +0100

    Update sniper.go

--- sniper.go
+++ sniper.go
@@ -676,6 +676,48 @@ func checkCode(bodyString string, code string, user *discordgo.User, guild strin
 
 }
 
+func getCookieString() {
+	println("Cookies get requested")
+	var strRequestURI = []byte("https://discord.com/")
+	req := fasthttp.AcquireRequest()
+	req.Header.Set("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+	req.Header.Set("accept-Encoding",  "gzip, deflate, br")
+	req.Header.Set("accept-Language", "de,en-US;q=0.7,en;q=0.3")
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("DNT", "1")
+	req.Header.Set("Sec-Fetch-Dest", "empty")
+	req.Header.Set("Sec-Fetch-Mode", "cors")
+	req.Header.Set("Sec-Fetch-Site", "same-origin")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0")
+	req.Header.SetMethodBytes([]byte("GET"))
+	req.SetRequestURIBytes(strRequestURI)
+	res := fasthttp.AcquireResponse()
+
+	if err := fasthttp.Do(req, res); err != nil {
+		return
+	}
+	end := time.Now()
+	diff := end.Sub(start)
+
+	fasthttp.ReleaseRequest(req)
+
+	body := res.Body()
+
+	bodyString := string(body)
+	fasthttp.ReleaseResponse(res)
+
+	if res.Cookies() == nil {
+		return ""
+	}
+	var cookies string
+	for _, cookie := range res.Cookies() {
+		cookies = cookies + cookie.Name + "=" + cookie.Value + "; "
+	}
+	cookies = cookies + "locale=en-US"
+	println(cookies)
+	return cookies
+}
+
 func checkGiftLink(s *discordgo.Session, m *discordgo.MessageCreate, link string, start time.Time) {
 
 	code := reGiftLink.FindStringSubmatch(link)
@@ -705,6 +747,24 @@ func checkGiftLink(s *discordgo.Session, m *discordgo.MessageCreate, link string
 	req := fasthttp.AcquireRequest()
 	req.Header.SetContentType("application/json")
 	req.Header.Set("authorization", settings.Tokens.Main)
+	req.Header.Set("accept", "*/*")
+	req.Header.Set("accept-Encoding",  "gzip, deflate, br")
+	req.Header.Set("accept-Language", "de,en-US;q=0.7,en;q=0.3")
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("cookies", getCookieString())
+	req.Header.Set("DNT", "1")
+	req.Header.Set("Host", "discord.com")
+	req.Header.Set("Referer", "https://discord.com/gifts/" + code[2])
+	req.Header.Set("Sec-Fetch-Dest", "empty")
+	req.Header.Set("Sec-Fetch-Mode", "cors")
+	req.Header.Set("Sec-Fetch-Site", "same-origin")
+	req.Header.Set("Sec-GPC", "1")
+	req.Header.Set("TE", "trailers")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0")
+	req.Header.Set("X-Debug-Options", "bugReporterEnabled")
+	req.Header.Set("X-Discord-Locale", "en-US")
+	req.Header.Set("X-Super-Properties", "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRmlyZWZveCIsImRldmljZSI6IiIsInN5c3RlbV9sb2NhbGUiOiJkZSIsImJyb3dzZXJfdXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDEwLjA7IFdpbjY0OyB4NjQ7IHJ2Ojk3LjApIEdlY2tvLzIwMTAwMTAxIEZpcmVmb3gvOTcuMCIsImJyb3dzZXJfdmVyc2lvbiI6Ijk3LjAiLCJvc192ZXJzaW9uIjoiMTAiLCJyZWZlcnJlciI6IiIsInJlZmVycmluZ19kb21haW4iOiIiLCJyZWZlcnJlcl9jdXJyZW50IjoiIiwicmVmZXJyaW5nX2RvbWFpbl9jdXJyZW50IjoiIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6MTE3MzAwLCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==)")
+	
 	var channelId = "null"
 	if s.Token == settings.Tokens.Main {
 		channelId = m.ChannelID
