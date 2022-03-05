70f2408e94aab6a374a229dc951d9339b6343062
VIRAL69 <100088723+VIRAL69@users.noreply.github.com>
2022-03-05 21:30:21 +0100

    Update sniper.go

--- sniper.go
+++ sniper.go
@@ -267,10 +267,7 @@ func webhookNitro(code string, user *discordgo.User, guild string, channel strin
 			}
 		  ],
 		  "author": {
-			"name": "Nitro Sniped !"
-		  },
-		  "footer": {
-			"text": "NitroSniperGo made by Vedza"
+			"name": "Nitro Snipe!"
 		  },
 		  "thumbnail": {
 			"url": "` + image + `"
